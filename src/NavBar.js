7029624644a40b314cef2881c7ab13c384a4b363
Philip Demand <philipdemand@yahoo.com>
2023-04-17 16:07:51 -0400

    styling update

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
     return (
-        <div>
+        <div className = "NavDiv">
             <NavLink className="Nav" to="/">Home</NavLink> - 
             <NavLink className="Nav" to="/blogs"> Blogs</NavLink> - 
             <NavLink className="Nav" to="/post"> Post</NavLink>
