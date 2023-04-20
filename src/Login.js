dd10c7fe7ddd574d92372cb26246d6d7fe3a4441
Philip Demand <philipdemand@yahoo.com>
2023-04-19 22:42:36 -0400

    login component visual

--- src/Login.js
+++ src/Login.js
@@ -14,6 +14,7 @@ function Login({ showForm }) {
                     type="password"
                     />
                 </label>
+                <br></br>
                 <button>Login</button>
             </form>
         </div>
