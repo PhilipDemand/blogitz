f3a8188970dbbc30a022e3289ebc152736e37013
Philip Demand <philipdemand@yahoo.com>
2023-04-19 22:34:45 -0400

    filter by likes

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,29 @@
+import React, { useState } from 'react';
 import BlogCard from './BlogCard'
 
 function Home({ blogs, handlePlusLike }) {
+
+    const [atLeastLikes, setAtLeastLikes] = useState("")
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+      };
     
-    const sortedArray = blogs.sort((a, b) => b.id - a.id);
+    const filteredArray = blogs.filter(entry => entry.likes >= atLeastLikes)
     
+      const sortedArray = filteredArray.sort((a, b) => b.id - a.id);
+ 
     return (
         <div>
+             <div className="card">
+                <form onSubmit={handleSubmit}>
+                <label>
+                    Number
+                    <input type="text" value={atLeastLikes} onChange={(e) => setAtLeastLikes(e.target.value)} />
+                </label>
+                <br />
+                </form>
+            </div>
             {sortedArray.map(blogObj => (
                 <BlogCard key={blogObj.id} blogObj={blogObj} handlePlusLike={handlePlusLike}/>
             ))}
