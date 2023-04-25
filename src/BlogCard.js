2d9e3af77200306da10440a4096ff89946cb0837
Philip Demand <philipdemand@yahoo.com>
2023-04-25 17:36:23 -0400

    updated sort logic

--- src/BlogCard.js
+++ src/BlogCard.js
@@ -23,7 +23,7 @@ function sendPlusLike() {
 function createStampMessage(string) {
   
     const year = string.slice(0,4)
-    const monthNumber = string.slice(4,6)
+    const month = string.slice(4,6)
     const day = string.slice(6,8)
     const hour = string.slice(8,10)
     const minute = string.slice(10,12)
@@ -31,31 +31,15 @@ function createStampMessage(string) {
   
     const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   
-    const monthName = monthNames[monthNumber - 1];
+    const monthName = monthNames[month - 1];
   
     function parseHourNumber(string) {
-      if (string === "00") {
-        return 12
-      } else if (string === "01") {
-        return 1
-      } else if (string === "02") {
-        return 2
-      } else if (string === "03") {
-        return 3
-      } else if (string === "04") {
-        return 4
-      } else if (string === "05") {
-        return 5
-      } else if (string === "06") {
-        return 6
-      } else if (string === "07") {
-        return 7
-      } else if (string === "08") {
-        return 8
-      } else if (string === "09") {
-        return 9
-      } else return parseInt(string)
-    }
+        if (string === "00") {
+          return 12
+        } else {
+          return parseInt(string)
+        }
+      }
   
     const hourNumber = parseHourNumber(hour)
   
@@ -70,32 +54,15 @@ function createStampMessage(string) {
     const regularTimeNumber = regularTime(hourNumber)
   
     function antiPost(string) {
-      if (string === "00") {
-        return "AM"
-      } else if (string === "01") {
-        return "AM"
-      } else if (string === "02") {
-        return "AM"
-      } else if (string === "03") {
-        return "AM"
-      } else if (string === "04") {
-        return "AM"
-      } else if (string === "05") {
-        return "AM"
-      } else if (string === "06") {
-        return "AM"
-      } else if (string === "07") {
-        return "AM"
-      } else if (string === "08") {
-        return "AM"
-      } else if (string === "09") {
-        return "AM"
-      } else if (string === "10") {
-        return "AM"
-      } else if (string === "11") {
-        return "AM"
-      } else return "PM"
-    }
+
+        const parsedString = parseInt(string)
+    
+        if (parsedString < 12) {
+          return "AM"
+        } else {
+          return "PM"
+        }
+      }
   
     const halfOfDay = antiPost(hour)
   
